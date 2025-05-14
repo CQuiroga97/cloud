2bc716543a7754fdad71499a1b7709d1d4a76b06
CQuirogaTechD <cquiroga@techd.com>
2025-05-14 11:13:32 -0500

    first commit

--- cloud.py
+++ cloud.py
@@ -9,7 +9,7 @@ s3 = boto3.client('s3')
 BUCKET_NAME = 'imagenes-taller-cristhian'
 # Configuración RDS
 conn = psycopg2.connect(
-    host='imagenesdb2.czyqgkyssdoq.us-east-1.rds.amazonaws.com ',
+    host='imagenesdb2.czyqgkyssdoq.us-east-1.rds.amazonaws.com',
     database='imagenes',
     user='admin',
     password='!Cristian2396980'
